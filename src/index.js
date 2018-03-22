f517956761e50b6fee8593d809c780347a9942d1
Nikitos09 <nikita0999.99@mail.ru>
2018-03-22 22:22:01 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+k=0;var a=[];
+  nextPrime:
+  for (var i = 2; i < 250; i++) {
+
+    for (var j = 2; j < i; j++) {
+      if (i % j == 0) continue nextPrime;
+    }
+
+  a[k]=i;k++;
+  }
+  node=0;s=0;k=0;
+for (i=0;i<a.length ;i++){
+  if (base % a[i]==0){k=0;
+  while (base % a[i]==0){
+    base=base/a[i];
+    k++;
+
+nod=a[i];
+}}}
+
+
+  for (i=nod;i<=number;i= i*nod){
+      s=s+Math.floor(number/i);
+    }
+return Math.floor(s/k);
 }
